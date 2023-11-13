603d05780e15ee4b221ce06d7928070655e030ff
Zerooaa <markrances@iskolarngbayan.pup.edu.ph>
2023-11-13 19:08:50 +0800

    Fixed and Added Features

--- Finals/script.js
+++ Finals/script.js
@@ -49,6 +49,11 @@ function verifyForm2(){
             alert("Please Fill out the Course, Year, & Section Field")
             return false;
         }
+        let dayandtime1 = document.forms ["subjectForm1"] ["dayandtime1"].value;
+        if (dayandtime1 == ""){
+            alert("Please Fill out the Date and Time Field")
+            return false;
+        }
         let room1 = document.forms ["subjectForm1"] ["room1"].value;
         if (room1 == ""){
             alert("Please Fill out the Room Field")
@@ -73,6 +78,13 @@ function verifyForm2(){
 const btn3 = document.getElementById("btnAdd2");
 btn3.disabled=true;
 
+const input1 = document.getElementById("hoursenrolled");
+input1.disabled=true;
+const input2 = document.getElementById("unitandhoursadded");
+input2.disabled=true;
+const input3 = document.getElementById("totalunitandhoursenrolled");
+input3.disabled=true;
+
 function verifyForm3(){
     event.preventDefault();
 
@@ -88,6 +100,11 @@ function verifyForm3(){
         let cys2 = document.forms ["subjectForm2"] ["cys2"].value;
         if (cys2 == ""){
             alert("Please Fill out the Course, Year, & Section Field")
+            return false;
+        }
+        let dayandtime2 = document.forms ["subjectForm2"] ["dayandtime2"].value;
+        if (dayandtime2 == ""){
+            alert("Please Fill out the Date and Time Field")
             return false;
         }
         let room2 = document.forms ["subjectForm2"] ["room2"].value;
@@ -144,7 +161,7 @@ function initializeAddButton(btn, table, subjectInput, cysInput, datInput, roomI
         let room = roomInput.value;
         let units = unitsInput.value;
         let facultyname = facultyInput.value;
-    
+
         let template = `
             <tr>
                 <td>${subject}</td>
@@ -155,27 +172,38 @@ function initializeAddButton(btn, table, subjectInput, cysInput, datInput, roomI
                 <td>${facultyname}</td>
             </tr>`;
         table.innerHTML += template;
+
+        clearFields([subjectInput, cysInput, datInput, roomInput, unitsInput, facultyInput]);
     });
+}
+
+function clearFields(inputs) {
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].value = '';
     }
-    
-    const btnAdd1 = document.querySelector('#btnAdd1');
-    const table1 = document.querySelector('#table1');
-    const subjectInput1 = document.querySelector('#subject1');
-    const cysInput1 = document.querySelector('#cys1');
-    const datInput1 = document.querySelector('#dayandtime1');
-    const roomInput1 = document.querySelector('#room1');
-    const unitsInput1 = document.querySelector('#units1');
-    const facultyInput1 = document.querySelector('#facultyname1');
-    
-    initializeAddButton(btnAdd1, table1, subjectInput1, cysInput1, datInput1, roomInput1, unitsInput1, facultyInput1);
-    
-    const btnAdd2 = document.querySelector('#btnAdd2');
-    const table2 = document.querySelector('#table2');
-    const subjectInput2 = document.querySelector('#subject2');
-    const cysInput2 = document.querySelector('#cys2');
-    const datInput2 = document.querySelector('#dayandtime2');
-    const roomInput2 = document.querySelector('#room2');
-    const unitsInput2 = document.querySelector('#units2');
-    const facultyInput2 = document.querySelector('#facultyname2');
-    
-    initializeAddButton(btnAdd2, table2, subjectInput2, cysInput2, datInput2, roomInput2, unitsInput2, facultyInput2);
+}
+
+const btnAdd1 = document.querySelector('#btnAdd1');
+const table1 = document.querySelector('#table1');
+const subjectInput1 = document.querySelector('#subject1');
+const cysInput1 = document.querySelector('#cys1');
+const datInput1 = document.querySelector('#dayandtime1');
+const roomInput1 = document.querySelector('#room1');
+const unitsInput1 = document.querySelector('#units1');
+const facultyInput1 = document.querySelector('#facultyname1');
+
+initializeAddButton(btnAdd1, table1, subjectInput1, cysInput1, datInput1, roomInput1, unitsInput1, facultyInput1);
+
+
+const btnAdd2 = document.querySelector('#btnAdd2');
+const table2 = document.querySelector('#table2');
+const subjectInput2 = document.querySelector('#subject2');
+const cysInput2 = document.querySelector('#cys2');
+const datInput2 = document.querySelector('#dayandtime2');
+const roomInput2 = document.querySelector('#room2');
+const unitsInput2 = document.querySelector('#units2');
+const facultyInput2 = document.querySelector('#facultyname2');
+
+initializeAddButton(btnAdd2, table2, subjectInput2, cysInput2, datInput2, roomInput2, unitsInput2, facultyInput2);
+
+
