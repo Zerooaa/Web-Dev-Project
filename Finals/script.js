34019eff5dffb2c4f7d20ff22efe584f315e6909
Pat2531 <patrickjaysonocayao@iskolarngbayan.pup.edu.ph>
2023-11-21 17:09:10 +0800

    Button, Button Design, Alert in submit

--- Finals/script.js
+++ Finals/script.js
@@ -42,3 +42,16 @@ const unitsInput1 = document.querySelector('#units1');
 const facultyInput1 = document.querySelector('#facultyname1');
 
 initializeAddButton(btnAdd1, table1, subjectInput1, cysInput1, dayInput1, timeInput1, roomInput1, unitsInput1, facultyInput1);
+
+
+document.getElementById('myForm1').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    alert('Data has been submitted!');
+});
+
+document.getElementById('myForm2').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    alert('Data has been submitted!');
+});
