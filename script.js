0c1c4d8afff54192d19148e5c9cc44fe08f3ad7d
Pat2531 <patrickjaysonocayao@iskolarngbayan.pup.edu.ph>
2023-12-05 22:42:33 +0800

    Index0 size of the background fixed / Change of color to lighter maroon / Change date to day / course year and section to accepted change
    CHANGE CHANGE

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function initializeAddButton(btn, table, subjectInput, cysInput, dayInput, timeInput, roomInput, unitsInput, facultyInput) {
+function initializeAddButton(btn, table, subjectInput, cInput, ysInput, dayInput, timeInput, roomInput, unitsInput, facultyInput) {
     btn.addEventListener('click', (event) => {
         event.preventDefault();
         let subject = subjectInput.value;
-        let cys = cysInput.value;
+        let c = cInput.value;
+        let ys = ysInput.value
         let day = dayInput.value;
         let time = timeInput.value;
         let room = roomInput.value;
@@ -12,7 +13,8 @@ function initializeAddButton(btn, table, subjectInput, cysInput, dayInput, timeI
         let template = `
             <tr>
                 <td>${subject}</td>
-                <td>${cys}</td>
+                <td>${c}</td>
+                <td>${ys}</td>
                 <td>${day}</td>
                 <td>${time}</td>
                 <td>${room}</td>
@@ -21,7 +23,7 @@ function initializeAddButton(btn, table, subjectInput, cysInput, dayInput, timeI
             </tr>`;
         table.innerHTML += template;
 
-        clearFields([subjectInput, cysInput, dayInput, timeInput, roomInput, unitsInput, facultyInput]);
+        clearFields([subjectInput, cInput, ysInput, dayInput, timeInput, roomInput, unitsInput, facultyInput]);
     });
 }
 
@@ -34,14 +36,15 @@ function clearFields(inputs) {
 const btnAdd1 = document.querySelector('#btnAdd1');
 const table1 = document.querySelector('#table1');
 const subjectInput1 = document.querySelector('#subject1');
-const cysInput1 = document.querySelector('#cys1');
+const cInput1 = document.querySelector('#c1');
+const ysInput1 = document.querySelector('#ys1');
 const dayInput1 = document.querySelector('#day1');
 const timeInput1 = document.querySelector('#time1');
 const roomInput1 = document.querySelector('#room1');
 const unitsInput1 = document.querySelector('#units1');
 const facultyInput1 = document.querySelector('#facultyname1');
 
-initializeAddButton(btnAdd1, table1, subjectInput1, cysInput1, dayInput1, timeInput1, roomInput1, unitsInput1, facultyInput1);
+initializeAddButton(btnAdd1, table1, subjectInput1, cInput1, ysInput1, dayInput1, timeInput1, roomInput1, unitsInput1, facultyInput1);
 
 
 function showAlert() {
--- script2.js
+++ script2.js
@@ -1,8 +1,8 @@
-function initializeAddButton(btn, table, subjectInput, cysInput, dayInput, timeInput, roomInput, unitsInput, facultyInput) {
+function initializeAddButton(btn, table, subjectInput, sInput, dayInput, timeInput, roomInput, unitsInput, facultyInput) {
     btn.addEventListener('click', (event) => {
         event.preventDefault();
         let subject = subjectInput.value;
-        let cys = cysInput.value;
+        let s = sInput.value;
         let day = dayInput.value;
         let time = timeInput.value;
         let room = roomInput.value;
@@ -12,7 +12,7 @@ function initializeAddButton(btn, table, subjectInput, cysInput, dayInput, timeI
         let template = `
             <tr>
                 <td>${subject}</td>
-                <td>${cys}</td>
+                <td>${s}</td>
                 <td>${day}</td>
                 <td>${time}</td>
                 <td>${room}</td>
@@ -21,7 +21,7 @@ function initializeAddButton(btn, table, subjectInput, cysInput, dayInput, timeI
             </tr>`;
         table.innerHTML += template;
 
-        clearFields([subjectInput, cysInput, dayInput, timeInput, roomInput, unitsInput, facultyInput]);
+        clearFields([subjectInput, sInput, dayInput, timeInput, roomInput, unitsInput, facultyInput]);
     });
     }
 
@@ -34,14 +34,14 @@ function clearFields(inputs) {
     const btnAdd2 = document.querySelector('#btnAdd2');
     const table2 = document.querySelector('#table2');
     const subjectInput2 = document.querySelector('#subject2');
-    const cysInput2 = document.querySelector('#cys2');
+    const sInput2 = document.querySelector('#s2');
     const dayInput2 = document.querySelector('#day2');
     const timeInput2 = document.querySelector('#time2');
     const roomInput2 = document.querySelector('#room2');
     const unitsInput2 = document.querySelector('#units2');
     const facultyInput2 = document.querySelector('#facultyname2');
 
-    initializeAddButton(btnAdd2, table2, subjectInput2, cysInput2, dayInput2, timeInput2, roomInput2, unitsInput2, facultyInput2);
+    initializeAddButton(btnAdd2, table2, subjectInput2, sInput2, dayInput2, timeInput2, roomInput2, unitsInput2, facultyInput2);
 
 function showAlert() {
     alert("Form submitted!");
